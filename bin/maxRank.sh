729acd84a7f84ad374557b55a6d24c6c64166b47
Marco <10561636@polimi.it>
2022-05-11 13:35:19 +0200

    Removed unused src/headers

--- bin/maxRank.sh
+++
@@ -1,9 +0,0 @@
-#! bin/bash
-
-#$ -j y
-#$ -cwd
-#$ -m e
-#$ -q "short.q"
-
-./myQuadTree -p 4096 -d 4 -i idx4 -f ../DataGen/RectUni4D100K.txt -q Queries100K.txt -r 20 -m AA -h 7 -t 0 -o 0 > OutputUni4D100K.txt
-
